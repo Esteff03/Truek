50adcce5485e8abdc6eed4731e0965ee1f2ed33e
Your Name <you@example.com>
2025-01-16 10:36:55 +0100

    Primer commit Sergio

--- app/src/main/java/com/truek/SplashActivity.java
+++ app/src/main/java/com/truek/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.truek;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,18 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Temporizador para redirigir al siguiente Activity
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000); // 3000 ms = 3 segundos
     }
 }
