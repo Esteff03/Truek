d41f1cbe43067a204259c7e56eb41717c4b008b5
Benjamin Rocha <brcj16@gmail.com>
2025-01-30 10:45:47 +0100

    commit prueba

--- app/src/main/java/com/truek/MainActivity.java
+++ app/src/main/java/com/truek/MainActivity.java
@@ -1,16 +1,12 @@
 package com.truek;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
