b47d878bc7ab42d9fb524ee0c558b6a0e2d1e314
Benjamin Rocha <brcj16@gmail.com>
2025-02-17 13:07:24 +0100

    marcar favoritos

--- app/src/main/java/com/truek/Producto.java
+++ app/src/main/java/com/truek/Producto.java
@@ -1,25 +1,24 @@
 package com.truek;
 
-public class Producto {
+import java.io.Serializable;
+
+import java.io.Serializable;
+
+public class Producto implements Serializable {
     private String nombre;
     private String precio;
-    private int imagenResId;
+    private int imagen;
+    private String descripcion;
 
-    public Producto(String nombre, String precio, int imagenResId) {
+    public Producto(String nombre, String precio, int imagen, String descripcion) {
         this.nombre = nombre;
         this.precio = precio;
-        this.imagenResId = imagenResId;
+        this.imagen = imagen;
+        this.descripcion = descripcion;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getPrecio() {
-        return precio;
-    }
-
-    public int getImagenResId() {
-        return imagenResId;
-    }
+    public String getNombre() { return nombre; }
+    public String getPrecio() { return precio; }
+    public int getImagen() { return imagen; }
+    public String getDescripcion() { return descripcion; }
 }
