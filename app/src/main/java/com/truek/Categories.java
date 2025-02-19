878ec6856fddc472325da080abf70ca01cdda27d
Benjamin Rocha <brcj16@gmail.com>
2025-02-19 13:02:29 +0100

    categories

--- app/src/main/java/com/truek/Categories.java
+++ app/src/main/java/com/truek/Categories.java
@@ -3,6 +3,7 @@ package com.truek;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -20,6 +21,8 @@ public class Categories extends BaseActivity {
         setContentView(R.layout.activity_categories);
 
         ImageView arrowBack = findViewById(R.id.arrow_back);
+        Button buttonmain = findViewById(R.id.button);
+
         arrowBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +32,18 @@ public class Categories extends BaseActivity {
                 finish(); // Cierra la actividad actual para que no se acumule en la pila
             }
         });
+
+        buttonmain.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Volver a MainActivity
+                Intent intent = new Intent(Categories.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
